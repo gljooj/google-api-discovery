ccf3fbfc5e886fda007b1dad0a0ea0a4458d0c98
Gabriel Jesus <gabrielthe13@hotmail.com>
2021-05-31 11:25:35 -0300

    creating model

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Customer(models.Model):
+    class Gender(models.TextChoices):
+        MALE = 'MAL', _('Male')
+        FEMALE = 'FEM', _('Female')
+        UNDEFINED = 'UND', _('Undefined')
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=144)
+    gender = models.CharField(
+        max_length=3,
+        choices=Gender.choices,
+        default=Gender.MALE,
+    )
+    company = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
