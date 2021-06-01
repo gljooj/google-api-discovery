4b5c8e62edefaa56484324c8845669419c2535ee
Gabriel Jesus <gabrielthe13@hotmail.com>
2021-06-01 17:07:43 -0300

    Changes

--- customer/models.py
+++ customer/models.py
@@ -4,21 +4,23 @@ from django.utils.translation import gettext_lazy as _
 
 class Customer(models.Model):
     class Gender(models.TextChoices):
-        MALE = 'MAL', _('Male')
-        FEMALE = 'FEM', _('Female')
+        MALE = 'Male', _('Male')
+        FEMALE = 'Female', _('Female')
         UNDEFINED = 'UND', _('Undefined')
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=144)
     gender = models.CharField(
-        max_length=3,
+        max_length=6,
         choices=Gender.choices,
         default=Gender.MALE,
     )
     company = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
 
     def __str__(self):
         return self.email
