265222846b2a3259d3ef0534ffb552622379448c
Gabriel Jesus <gabrielthe13@hotmail.com>
2021-06-01 17:07:22 -0300

    context

--- customer/infrastructure/google_maps/context/google_context_api.py
+++ customer/infrastructure/google_maps/context/google_context_api.py
@@ -1,15 +1,15 @@
 from customer.context.context_api import ContextAPI
 import requests
-
+import os
 
 class GoogleContextAPI(ContextAPI):
     def __init__(self):
         self.base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
-        self.token = ''
+        self.token = os.getenv("token")
         self.params = {'key': self.authentication()}
 
     def authentication(self) -> str:
-        return 'AIzaSyDc7QuWmkb6PcJFuYKySnj6KbW3B0Y4Tfc'
+        return self.token
 
     def __bind_params(self, request):
         return self.params.update(request.payload)
@@ -19,4 +19,4 @@ class GoogleContextAPI(ContextAPI):
 
     def do_get(self, request):
         self.__bind_params(request)
-        return requests.get(url=self.base_url, params=self.params,verify=False)
+        return requests.get(url=self.base_url, params=self.params, verify=False)
