1b857159181e4a3dde2d0d0812a89f2c738155c3
Gabriel Jesus <gabrielthe13@hotmail.com>
2021-06-02 21:14:27 -0300

    documentation

--- oowlish/settings.py
+++ oowlish/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
 INSTALLED_APPS = [
     'customer',
     'rest_framework',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
