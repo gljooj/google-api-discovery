7e1f2bd81768e011852f6547a5c101abb170ac61
Gabriel Jesus <gabrielthe13@hotmail.com>
2021-06-15 15:52:17 -0300

    Rename

--- google-api-discovery/asgi.py
+++ google-api-discovery/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for oowlish project.
+ASGI config for google-api-discovery project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oowlish.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'google-api-discovery.settings')
 
 application = get_asgi_application()
--- google-api-discovery/wsgi.py
+++ google-api-discovery/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for oowlish project.
+WSGI config for google-api-discovery project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oowlish.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'google-api-discovery.settings')
 
 application = get_wsgi_application()
